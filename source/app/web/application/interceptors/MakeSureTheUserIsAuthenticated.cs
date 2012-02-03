044561ffa03bb777ff09d24bb92414fba437e830
swilliams <swilliams@daxko.com>
2012-02-03 13:21:23 -0600

    Pushing new changes

--- source/app/web/application/interceptors/MakeSureTheUserIsAuthenticated.cs
+++ source/app/web/application/interceptors/MakeSureTheUserIsAuthenticated.cs
@@ -5,15 +5,20 @@ namespace app.web.application.interceptors
   public class MakeSureTheUserIsAuthenticated<RequestToForwardToIfNotAuthenticated> : IInterceptWithoutForwardingTheCall
   {
     IRedirect redirect;
+      GetTheCurrentPrincipal the_current_principal;
 
-    public MakeSureTheUserIsAuthenticated(IRedirect redirect)
-    {
-      this.redirect = redirect;
-    }
+      public MakeSureTheUserIsAuthenticated(IRedirect redirect, GetTheCurrentPrincipal the_current_principal)
+      {
+          this.redirect = redirect;
+          this.the_current_principal = the_current_principal;
+      }
 
-    public void process(IProvideDetailsToCommands request)
+      public void process(IProvideDetailsToCommands request)
     {
-      
+        var principal = the_current_principal();
+        if (principal.Identity.IsAuthenticated)
+            return;
+        redirect.to<RequestToForwardToIfNotAuthenticated>();
     }
   }
 }
