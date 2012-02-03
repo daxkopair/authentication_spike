964da20dc7bce56da56e5551ee4e3c79795d05c5
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-02-03 11:13:38 -0700

    Updated

--- source/app.specs/ObjectExtensionsSpecs.cs
+++ source/app.specs/ObjectExtensionsSpecs.cs
@@ -1,4 +1,5 @@
-﻿using Machine.Specifications;
+﻿using System;
+using Machine.Specifications;
 using app.utility;
 using developwithpassion.specifications.rhinomocks;
 using developwithpassion.specifications.extensions;
@@ -23,6 +24,22 @@ namespace app.specs
 
         It should_return_the_casted_instance = () =>
           (the_item).cast_to<SomeItem>().ShouldBeAn<SomeItem>();
+
+        static object the_item;
+      }
+      public class and_the_cast_is_invalid
+      {
+        Establish c = () =>
+        {
+          the_item = new SomeItem();
+        };
+
+        Because b = () =>
+          spec.catch_exception(() => the_item.cast_to<int>());
+
+
+        It should_get_an_invalid_cast_exception = () =>
+          spec.exception_thrown.ShouldBeAn<InvalidCastException>();
 
         static object the_item;
       }
