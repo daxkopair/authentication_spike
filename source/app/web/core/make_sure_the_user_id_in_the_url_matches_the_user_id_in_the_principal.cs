648dd4d6491db79e90465c8c10310e151eb70d32
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-02-03 13:05:16 -0700

    Updated

--- source/app/web/core/make_sure_the_user_id_in_the_url_matches_the_user_id_in_the_principal.cs
+++ source/app/web/core/make_sure_the_user_id_in_the_url_matches_the_user_id_in_the_principal.cs
@@ -1,10 +1,26 @@
 ﻿namespace app.web.core
 {
-    public class make_sure_the_user_id_in_the_url_matches_the_user_id_in_the_principal<RequestToForwardToIfIdsAreDifferent> : IInterceptWithoutForwardingTheCall
+  public class make_sure_the_user_id_in_the_url_matches_the_user_id_in_the_principal<RequestToForwardToIfIdsAreDifferent> :
+    IInterceptWithoutForwardingTheCall
+  {
+    GetTheCurrentUrl_Behaviour current_url;
+    IsAuthorizedUrl_Behaviour url_is_authorized;
+    IRedirect redirect;
+
+    public make_sure_the_user_id_in_the_url_matches_the_user_id_in_the_principal(GetTheCurrentUrl_Behaviour current_url,
+                                                                                 IsAuthorizedUrl_Behaviour
+                                                                                   url_is_authorized, IRedirect redirect)
     {
-        public void process(IProvideDetailsToCommands request)
-        {
-            
-        }
+      this.current_url = current_url;
+      this.url_is_authorized = url_is_authorized;
+      this.redirect = redirect;
     }
+
+    public void process(IProvideDetailsToCommands request)
+    {
+      if (url_is_authorized(current_url())) return;
+
+      redirect.to<RequestToForwardToIfIdsAreDifferent>();
+    }
+  }
 }
